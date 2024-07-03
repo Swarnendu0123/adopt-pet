c5044e5995cf116edb0c9d20af3126830d34a0a8
Swarnendu <clickswarnendu123@gmail.com>
2024-07-03 20:54:59 +0530

    more tests

--- contracts/PerAdoption.sol
+++ contracts/PerAdoption.sol
@@ -5,6 +5,10 @@ pragma solidity ^0.8.9;
 contract PetAdoption{
     address public owner;
     uint public petIndex = 0;
+    uint[] public allAdoptedPets;
+
+    mapping(uint => address) public petToOwner;
+    mapping(address => uint[]) public ownerToPets;
 
     constructor(uint initialPetIndex){
         owner = msg.sender;
@@ -14,6 +18,13 @@ contract PetAdoption{
     function addPet() public {
         require(msg.sender == owner, "You are not a contract owner");
         petIndex++;
+    }
+
+    function adoptPet(uint adoptIdx) public {
+        require(adoptIdx < petIndex, "Pet index out of range");
+        require(petToOwner[adoptIdx] == address(0), "Pet already adopted");
+        petToOwner[adoptIdx] = msg.sender;
+        ownerToPets[msg.sender].push(adoptIdx);
     }
 
     function getOwner() public view returns(address){
--- test/PerAdoption.js
+++ test/PerAdoption.js
@@ -3,24 +3,31 @@ const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers")
 
 describe("PetAdoption", function () {
 
-    async function deployContract(){
+    async function deployContract() {
         const PET_COUNT = 5;
-        const [owner, account2] = await ethers.getSigners();
+        const ADOPTED_PET_INDEX = 2;
+        const [owner, account2, account3] = await ethers.getSigners();
         const PetAdoption = await ethers.getContractFactory("PetAdoption");
         const contract = await PetAdoption.deploy(PET_COUNT);
 
-        return {owner, contract, account2, petsAddedCount: PET_COUNT};
+        await contract.connect(account3).adoptPet(ADOPTED_PET_INDEX);
+        return {
+            owner, account2, account3,
+            contract,
+            petsAddedCount: PET_COUNT,
+            adoptedPetIndex: ADOPTED_PET_INDEX
+        };
     }
 
     describe("Deployment", function () {
         it("Should set the right owner", async function () {
-            const {owner, contract} = await loadFixture(deployContract);
+            const { owner, contract } = await loadFixture(deployContract);
             const contractOwner = await contract.owner();
             expect(contractOwner).to.equal(owner.address);
         });
 
-        it("Shoult return the right owner", async function(){
-            const {owner, contract} = await loadFixture(deployContract);
+        it("Shoult return the right owner", async function () {
+            const { owner, contract } = await loadFixture(deployContract);
             const contractOwner = await contract.getOwner();
             expect(contractOwner).to.equal(owner.address);
         });
@@ -28,18 +35,32 @@ describe("PetAdoption", function () {
 
 
     describe("Add pet function", function () {
-        it("Should revert the right error in case of other account", async function() {
-            const {owner, contract, account2} = await loadFixture(deployContract);
+        it("Should revert the right error in case of other account", async function () {
+            const { owner, contract, account2 } = await loadFixture(deployContract);
 
             await expect(contract.connect(account2).addPet()).to.be.revertedWith("You are not a contract owner");
         });
 
-        it("Should increse pet index", async function() {
-            const { contract, petsAddedCount} = await loadFixture(deployContract);
-            
+        it("Should increse pet index", async function () {
+            const { contract, petsAddedCount } = await loadFixture(deployContract);
+
             await contract.addPet();
             const returnedPetIndex = await contract.petIndex();
-            expect(returnedPetIndex).to.equal(petsAddedCount+1)
+            expect(returnedPetIndex).to.equal(petsAddedCount + 1)
+        });
+    });
+
+    describe("Adopt pet", function () {
+        it("Should revert with index out of range", async function () {
+            const { contract, petsAddedCount } = await loadFixture(deployContract);
+
+            await expect(contract.adoptPet(5)).to.be.revertedWith("Pet index out of range");
+            await expect(contract.adoptPet(-1)).to.be.rejectedWith("value out-of-bounds");
+        })
+
+        it("Should revert with pet already adopted", async function () {
+            const { contract, adoptedPetIndex, account2 } = await loadFixture(deployContract);
+            await expect(contract.connect(account2).adoptPet(adoptedPetIndex)).to.be.revertedWith("Pet already adopted");
         });
     });
 });
